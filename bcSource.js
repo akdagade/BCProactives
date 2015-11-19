6a46805bcad6991d8dfea3fbc2e23ddb617a8904
Akshay Dagade <akdagade@gmail.com>
2015-11-19 16:21:02 +0530

    UI for source

---
+++ bcSource.js
@@ -0,0 +1,22 @@
+window.onload = function(){
+
+inputRad = document.forms.inputForm.inputType;
+inRadval = inputRad.value;
+
+tagRad = document.forms.tagForm.logicType;
+tagRadval = tagRad.value;
+
+}
+
+
+function inputFunc(){
+
+inRadval = inputRad.value;
+//alert(inputRad.value);
+}
+
+function tagFunc(){
+
+tagRadval = tagRad.value;
+//alert(tagRadval);
+}
