477c4de8d6faceff07fbf9b2a60a5e13f3fb3631
Akshay Dagade <akdagade@gmail.com>
2015-11-20 02:36:17 +0530

    Working on target2

--- bcTarget.js
+++ bcTarget.js
@@ -9,14 +9,16 @@ else {
 }
 
 tags= previewString.split('!@#$%');
-
+alert(tags.length);
 var finalStr = "";
+
 for(var i=0;i<tags.length;i++) {
 
-finalStr += "<div id='adTag" + i + "' style='display:block;'><frame>" + tags[i] + "</frame><div id='adTagdiv"+ i +"'>adTagRep"+ i +"</div></div>" 
+finalStr = "<div id='adTag" + i + "' style='display:block;margin-top:30px;background-color:#c0c0c0;padding:20px;border-radius:15px'><frame>" + tags[i] + "</frame><textarea style='float:right;max-height:300px;max-width:500px;height:300px;width:500px'>"+ tags[i] +"</textarea><h4 style='margin-bottom:10px;margin-left:10px;'>Call Flow :</h4><div style='margin-top:5px;height:200px;width:1300px;margin-left:10px;overflow:scroll;background-color:#efefef;border:1px solid #8585e0;border-radius:10px' id='adTagdiv"+ i +"'></div></div>" 
+alert(finalStr);
+document.write(finalStr);
 
 }
-document.write(finalStr);
 
 
 window.onload = function(){
@@ -27,21 +29,24 @@ window.onload = function(){
 	alert(document.getElementById('adtest').getElementsByTagName("*")[i].src + '\n\n')
 }*/
 
-for (var i = 0; i < tags.length; i++) {
+	for (var i = 0; i < tags.length; i++) {	
 		tagChk = 'adTag'+i;
-		calls='';
+		calls='<ul style="margin-top:5px">';
 		if(document.getElementById(tagChk).getElementsByTagName("*").length > 0){
-	for (var j = 0; j < document.getElementById(tagChk).getElementsByTagName("*").length; j++) {
+			for (var j = 0; j < document.getElementById(tagChk).getElementsByTagName("*").length; j++) {
 		
-		if(document.getElementById(tagChk).getElementsByTagName("*")[j].src != null && document.getElementById(tagChk).getElementsByTagName("*")[j].src != '')
-			{
-				calls+=document.getElementById(tagChk).getElementsByTagName("*")[j].src + '\n\n'
+				if(document.getElementById(tagChk).getElementsByTagName("*")[j].src != null && document.getElementById(tagChk).getElementsByTagName("*")[j].src != '')
+					{
+						
+						calls+='<li>'+decodeURI(document.getElementById(tagChk).getElementsByTagName("*")[j].src)+'</li><br>'
 				//alert(document.getElementById(tagChk).getElementsByTagName("*")[j].src);
-			}
-	};
-	alert(calls);
-};
-}
+					}
+			};
+			calls+='</ul>';
+			document.getElementById('adTagdiv'+i).innerHTML=calls;
+			//alert(calls);
+		};
+	}
 };
 
 
