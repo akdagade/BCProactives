467fd841154103e98a0e078693f64e78bc47f85c
Akshay Dagade <akdagade@gmail.com>
2015-11-20 17:22:20 +0530

    Almost Done

--- bcTarget.js
+++ bcTarget.js
@@ -9,13 +9,12 @@ else {
 }
 
 tags= previewString.split('!@#$%');
-alert(tags.length);
-var finalStr = "";
+//alert(tags.length);
 
 for(var i=0;i<tags.length;i++) {
-
-finalStr = "<div id='adTag" + i + "' style='display:block;margin-top:30px;background-color:#c0c0c0;padding:20px;border-radius:15px'><frame>" + tags[i] + "</frame><textarea style='float:right;max-height:300px;max-width:500px;height:300px;width:500px'>"+ tags[i] +"</textarea><h4 style='margin-bottom:10px;margin-left:10px;'>Call Flow :</h4><div style='margin-top:5px;height:200px;width:1300px;margin-left:10px;overflow:scroll;background-color:#efefef;border:1px solid #8585e0;border-radius:10px' id='adTagdiv"+ i +"'></div></div>" 
-alert(finalStr);
+var finalStr = "";
+finalStr = "<div id='adTag" + i + "' style='margin-top:30px;background-color:#c0c0c0;padding:20px;border-radius:15px'><h2>AdTag Number : "+ (i+1) +"</h2>" + tags[i] + "<textarea id='adTagTxt"+ i +"' style='float:right;display:block;max-height:300px;max-width:500px;height:300px;width:500px'></textarea><h4 style='float:right;clear:right;margin-left:10px;margin-bottom:10px;margin-left:0px;display:table-cell'>Call Flow :</h4><div style='clear:right;margin-top:25px;height:200px;width:1300px;margin-left:0px;overflow:scroll;background-color:#efefef;border:1px solid #8585e0;border-radius:10px' id='adTagdiv"+ i +"'></div></div>" 
+//alert(finalStr);
 document.write(finalStr);
 
 }
@@ -28,24 +27,35 @@ window.onload = function(){
 	if(document.getElementById('adtest').getElementsByTagName("*")[i].src != null && document.getElementById('adtest').getElementsByTagName("*")[i].src != '')
 	alert(document.getElementById('adtest').getElementsByTagName("*")[i].src + '\n\n')
 }*/
-
+	
 	for (var i = 0; i < tags.length; i++) {	
+		flag = 0;
 		tagChk = 'adTag'+i;
-		calls='<ul style="margin-top:5px">';
+		
 		if(document.getElementById(tagChk).getElementsByTagName("*").length > 0){
+			calls='<ul style="margin-top:5px">';
 			for (var j = 0; j < document.getElementById(tagChk).getElementsByTagName("*").length; j++) {
 		
 				if(document.getElementById(tagChk).getElementsByTagName("*")[j].src != null && document.getElementById(tagChk).getElementsByTagName("*")[j].src != '')
 					{
-						
+						flag=1;			
 						calls+='<li>'+decodeURI(document.getElementById(tagChk).getElementsByTagName("*")[j].src)+'</li><br>'
 				//alert(document.getElementById(tagChk).getElementsByTagName("*")[j].src);
 					}
 			};
 			calls+='</ul>';
-			document.getElementById('adTagdiv'+i).innerHTML=calls;
+			
 			//alert(calls);
 		};
+
+		if(flag==0)
+			calls='<ul style="margin-top:5px"><li>No Calls/Invalid Tag</li></ul>';
+
+		if(document.getElementById('adTagdiv'+i) != null)
+				document.getElementById('adTagdiv'+i).innerHTML=calls;
+
+		if(document.getElementById('adTagTxt'+i) != null)
+				document.getElementById('adTagTxt'+i).value = tags[i];
 	}
 };
 
@@ -54,3 +64,8 @@ window.onload = function(){
 <frame style='border-width:0px;' frameborder='0'>"
  + previewString + 
 "</frame></div>";*/
+
+window.onerror = function(e, url, line){
+
+	alert(e + '\n\n' + url + line);
+}
