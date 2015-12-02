5ee19884845933f6a5bc72accdaa54f5e3447b50
Akshay Dagade <akdagade@gmail.com>
2015-12-03 01:13:35 +0530

    start_modify

--- bcTarget.js
+++ bcTarget.js
@@ -1,4 +1,35 @@
 
+function showInner(n1,n2){
+
+		if (document.readyState === "complete") 
+			{ 
+				//alert(n1+"  "+n2);
+			}
+		else
+			{
+				alert("Wait till page is loaded !!!");
+			}
+		
+	}
+
+function mailLog(n1,n2){
+
+		if (document.readyState === "complete") 
+			{ 
+				//alert(n1+"  "+n2);
+				 var conId = ''
+				 var bodyToSend = '\n~~~~~~~~~~~~~~~~~~~~~~ Logs ~~~~~~~~~~~~~~~~~~~\n\n'
+				 					+ escape(document.getElementById('myText').value)
+				 var link = "mailto:&body=" + ;
+
+	             window.location.href = link;
+			}
+		else
+			{
+				alert("Wait till page is loaded !!!");
+			}
+	}
+
 
 if(window.opener) {
          previewString = unescape(window.opener.document.getElementById('previewText').value);
@@ -20,9 +51,9 @@ for(var i=0;i<tags.length;i++) {
 	for (var j = 0; j < countValStr; j++) {
 		//tmpTag+=tags[i]+'\n'
 		if(j==0)
-			tmpTag +="<div id='adTag"+ i +"sub"+ j +"' style='margin-top:20px;display:inline;border:1px solid #000000;float:left'>" + tags[i] + "</div><textarea id='adTagTxt"+ i +"' style='float:right;display:block;max-height:300px;max-width:500px;height:300px;width:500px;border:2px solid #8585ff;'></textarea><h4 style='float:right;clear:both;margin-left:10px;margin-bottom:10px;margin-left:0px;display:table-cell'>Call Flow :</h4><div style='clear:both;margin-top:35px;height:300px;width:1300px;margin-left:0px;overflow-y: scroll; overflow-x:hidden;background-color:#efefef;border:2px solid #8585ff;border-radius:10px;word-wrap: break-word;' id='adTagdiv"+ i + "sub" + j +"'></div>"
+			tmpTag +="<div id='adTag"+ i +"sub"+ j +"' style='margin-top:20px;display:inline;border:1px solid #000000;float:left'>" + tags[i] + "</div><textarea id='adTagTxt"+ i +"' style='float:right;display:block;max-height:300px;max-width:500px;height:300px;width:500px;border:2px solid #8585ff;'></textarea><h4 style='float:left;clear:both;margin-left:10px;margin-bottom:10px;margin-top:20px;display:table-cell'>Call Flow :</h4><div style='clear:both;margin-top:35px;height:300px;width:1300px;margin-left:0px;overflow-y: scroll; overflow-x:hidden;background-color:#efefef;border:2px solid #8585ff;border-radius:10px;word-wrap: break-word;' id='adTagdiv"+ i + "sub" + j +"'></div><div style='margin-left:10px;margin-top:10px'><input type='button' style='margin-right:20px' id='ainner"+i+"sub"+j+"' value='Show innerHTML' onclick='showInner("+i+","+j+")'></input> <input type='button' style='' id='amail"+i+"sub"+j+"' value='Mail Log' onclick='mailLog("+i+","+j+")'></input></div>"
 		else
-			tmpTag +="<hr style='margin-top:20px'><hr><div id='adTag"+ i +"sub"+ j +"' style='margin-top:20px;border:1px solid #000000;float:left;'>" + tags[i] + "</div><h4 style='float:right;clear:right;margin-left:10px;margin-bottom:10px;margin-left:0px;display:table-cell;clear:both'>Call Flow :</h4><div style='margin-top:25px;height:300px;width:1300px;margin-left:0px;overflow-y: scroll; overflow-x:hidden;background-color:#efefef;border:2px solid #8585ff;border-radius:10px;clear:both;word-wrap: break-word;' id='adTagdiv"+ i + "sub" + j +"'></div>"
+			tmpTag +="<hr style='margin-top:20px'><hr><div id='adTag"+ i +"sub"+ j +"' style='margin-top:20px;border:1px solid #000000;float:left;'>" + tags[i] + "</div><h4 style='float:left;clear:right;margin-left:10px;margin-bottom:10px;margin-top:20px;display:table-cell;clear:both'>Call Flow :</h4><div style='margin-top:25px;height:300px;width:1300px;margin-left:0px;overflow-y: scroll; overflow-x:hidden;background-color:#efefef;border:2px solid #8585ff;border-radius:10px;clear:both;word-wrap: break-word;' id='adTagdiv"+ i + "sub" + j +"'></div><div style='margin-left:10px;margin-top:10px'><input type='button' style='margin-right:20px' id='ainner"+i+"sub"+j+"' value='Show innerHTML' onclick='showInner("+i+","+j+")'></input> <input type='button' style='' id='amail"+i+"sub"+j+"' value='Mail Log' onclick='mailLog("+i+","+j+")'></input></div>"
 		};
 //alert(tmpTag);
 	finalStr = "<div id='adTag" + i + "' style='margin-top:30px;background-color:#c0c0c0;padding:20px;border-radius:15px;'><h2>AdTag Number : "+ (i+1) +"</h2>" + tmpTag + "</div>" 
